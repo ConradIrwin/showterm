7580de996564eb05ee4258b30f4687d614975d5d
Conrad Irwin <conrad.irwin@gmail.com>
2012-10-07 23:51:02 -0700

    ++

---
+++ lib/showterm.rb
@@ -0,0 +1,61 @@
+require 'json'
+require 'tempfile'
+require 'net/https'
+
+module Showterm
+  extend self
+
+  def record!(*cmd)
+
+    scriptfile = Tempfile.new('showterm.script')
+    timingfile = Tempfile.new('showterm.time')
+
+    scriptfile.close(false)
+    timingfile.close(false)
+
+    args = []
+    if cmd.size > 0
+      args << '-c' + cmd.join(" ")
+    end
+
+    args << '-q'
+    args << '-t' + timingfile.path
+    args << scriptfile.path
+
+    puts "showterm is recording, quit when you're done."
+
+    unless system('script', *args)
+      raise "Could not run 'script', please check that it's installed"
+    end
+
+    puts 'Recorded'
+
+    [scriptfile.path, timingfile.path]
+  end
+
+  def upload!(scriptfile, timingfile, cols=80)
+    request = Net::HTTP::Post.new("/scripts")
+    request.set_form_data(:scriptfile => File.read(scriptfile),
+                          :timingfile => File.read(timingfile),
+                          :cols => cols)
+
+    response = http(request)
+    raise response.body unless Net::HTTPSuccess === response
+    puts response.body
+  end
+
+  private
+
+  def http(request)
+    connection = Net::HTTP.new("showterm.herokuapp.com", 443)
+    connection.use_ssl = true
+    connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    connection.open_timeout = 10
+    connection.read_timeout = 10
+    connection.start do |http|
+      http.request request
+    end
+  rescue Timeout::Error
+    raise "Could not connect to https://api.github.com/"
+  end
+end
---
+++ showterm.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = "showterm"
+  s.version = "0.1.pre.1"
+  s.platform = Gem::Platform::RUBY
+  s.author = "Conrad Irwin"
+  s.email = "conrad.irwin@gmail.com"
+  s.homepage = "http://github.com/Conradirwin/showterm"
+  s.summary = "Allows you to make screen casts of your terminal really easily"
+  s.description = "Integrates with showterm.io for awesome sharability."
+  s.files = `git ls-files`.split("\n")
+  s.require_path = "lib"
+  s.executables << 'showterm'
+end
